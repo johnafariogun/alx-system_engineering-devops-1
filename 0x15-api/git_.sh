0aba0a8a0970bf69b06e4b6a4eafa95c15d00bb6
Afariogun John Tolulope <afariogunjohn2502@gmail.com>
2023-04-24 13:22:26 -0700

    I think i have fixed the errors

--- 0x15-api/git_.sh
+++ 0x15-api/git_.sh
@@ -1,4 +1,17 @@
+#!/bin/bash
+
+# Check if a commit message is provided as an argument
+if [ -z "$1" ]; then
+  echo "Please provide a commit message as an argument."
+  exit 1
+fi
+
+# Add all changes to the staging area
 git add .
-git commit -m '$1'
+
+# Commit changes with the provided commit message
+git commit -m "$1"
+
+# Push changes to the default remote branch (usually "origin" and "main")
 git push
 
